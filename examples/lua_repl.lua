3eb0ed200c1fa524e596885f7e98b441ad8c2500
TSnake41 <TSnake@Outlook.fr>
2020-04-01 13:16:25 +0200

    Tiny change

--- examples/lua_repl.lua
+++ examples/lua_repl.lua
@@ -1,6 +1,6 @@
 rl.SetConfigFlags(rl.FLAG_VSYNC_HINT)
 
-rl.InitWindow(800, 450, "raylib [core] example - basic window")
+rl.InitWindow(800, 450, "raylib [lua] example - repl")
 
 while not rl.WindowShouldClose() do
 	rl.BeginDrawing()
